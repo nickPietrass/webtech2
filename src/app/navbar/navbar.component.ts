cce3b41b686a3bfc66065671242d8f7b858947b0
Nick <nick.pietrass@gmail.com>
2018-06-01 12:25:27 +0200

    initial

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  constructor() { }
+  pages: Array<String> = [];
+  ngOnInit() {
+
+    // test data
+
+    this.pages.push('test1');
+    this.pages.push('test2');
+
+
+  }
+
+}
+
+/* subcategory
+ <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+          <span>Saved reports</span>
+          <a class="d-flex align-items-center text-muted" href="#">
+            <span data-feather="plus-circle"></span>
+          </a>
+        </h6>
+
+  subcategory list
+  <ul class="nav flex-column mb-2">
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file-text"></span>
+              Current month
+            </a>
+          </li>
+  </ul>
+*/
---
+++ src/app/navtop/navtop.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navtop',
+  templateUrl: './navtop.component.html',
+  styleUrls: ['./navtop.component.css']
+})
+export class NavtopComponent implements OnInit {
+
+  constructor() { }
+  loggedIn = false;
+
+  ngOnInit() {
+  }
+
+}
