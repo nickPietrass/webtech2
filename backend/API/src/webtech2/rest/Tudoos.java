1f1ee7885750b1cccd079ea997de333256ba742d
Panossa2 <nomail@bla.com>
2018-06-28 14:49:19 +0200

    Backend init

---
+++ backend/API/src/webtech2/rest/Tudoos.java
@@ -0,0 +1,40 @@
+package webtech2.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by Ilja on 26.06.2018.
+ */
+@ApplicationPath("/data")
+@Path("/tudoos")
+public class Tudoos extends Application{
+
+    @GET
+    @Path("/") //geht auch ""?
+    @Produces(MediaType.APPLICATION_JSON)
+    public Object getUser(@QueryParam("id") String id){
+        //Call to JPA with id.
+        return "NONE";
+    }
+    
+    @POST
+    @Path("/create")
+    public Response registerUser(){
+        return Response.status(401).build();
+    }
+    
+    @DELETE
+    @Path("/remove")
+    public Response removeUser(@QueryParam("id") String id){
+        return Response.status(401).build();
+    }
+}
---
+++ backend/API/src/webtech2/rest/Users.java
@@ -0,0 +1,46 @@
+package webtech2.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by Ilja on 26.06.2018.
+ */
+@ApplicationPath("/data")
+@Path("/users")
+public class Users extends Application{
+
+    @GET
+    @Path("/") //geht auch ""?
+    @Produces(MediaType.APPLICATION_JSON)
+    public Object getUser(@QueryParam("id") String id){
+        //Call to JPA with id.
+        return "NONE";
+    }
+
+    @POST
+    @Path("/login")
+    public Response loginUser(){
+        return Response.status(401).build();
+    }
+    
+    @POST
+    @Path("/register")
+    public Response registerUser(){
+        return Response.status(401).build();
+    }
+    
+    @DELETE
+    @Path("/remove")
+    public Response removeUser(){
+        return Response.status(401).build();
+    }
+}
