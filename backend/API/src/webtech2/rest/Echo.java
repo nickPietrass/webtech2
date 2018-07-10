9ba96ec0f259bdf5db5723481855706a53925bfd
Panossa2 <nomail@bla.com>
2018-07-10 14:17:20 +0200

    crime

--- backend/API/src/webtech2/rest/Echo.java
+++ backend/API/src/webtech2/rest/Echo.java
@@ -1,16 +1,20 @@
 package webtech2.rest;
 
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.POST;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
 
-@ApplicationPath("/data")
-@Path("/echo")
+/**
+ * Simple ping/pong test.
+ * @author Ilja
+ */
+@ApplicationPath("")
+@Path("")
 public class Echo extends Application{
 	
-	@POST
+	@GET
     @Path("/ping")
     public Response registerUser(){
         return Response.ok("Pong!").build();
--- backend/API/src/webtech2/rest/Tudoos.java
+++ backend/API/src/webtech2/rest/Tudoos.java
@@ -4,6 +4,7 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -30,6 +31,23 @@ public class Tudoos extends Application{
     @Path("/create")
     public Response registerUser(){
         return Response.status(401).build();
+    }
+    
+    
+    /**
+     * Is able to change permissions up to removing a user/group completely.
+     * Examples: 
+     * 1. User with level 0 -> user has no more permissions now.
+     * 2. Group with level 2 -> all users of the given group have edit permissions now.
+     *  
+     * @param permLevel {@link webtech2.rest.auth.permission.PermissionLevel}
+     * @param targetID ID of the target (group ID or user login name).
+     * @return 200 if possible, 400 else.
+     */
+    @PUT
+    @Path("/updatePermission")
+    public Response updatePermission(@QueryParam("tudooID") String tudooID, @QueryParam("targetID") String targetID, @QueryParam("permissionLevel") String permLevel) {
+    	return Response.ok("").build();
     }
     
     @DELETE
