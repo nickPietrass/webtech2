3822d81c30965254bec52373d534853de178c583
We, Wayne <wayne.we@udo.edu>
2018-07-13 22:00:10 +0200

    updated registerNewTudoo method

--- backend/API/src/webtech2/jpa/TudooApp.java
+++ backend/API/src/webtech2/jpa/TudooApp.java
@@ -1,10 +1,15 @@
 package webtech2.jpa;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.UUID;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
@@ -28,21 +33,39 @@ public class TudooApp {
 		emf.close();
 	}
 	
-	public TudooGroup registerNewTodoo(User owner) throws NoDBEntryException {
+	public Tudoo registerNewTodoo(String title, String content, String loginName) throws NoDBEntryException {
 		EntityManager em = emf.createEntityManager();
 		App app = new App();
-		
-		User managedUser = app.getUserByLoginName(owner.getLoginName());
+    	
+    	if (app.userIsNotInDB(loginName)) {
+    		throw new NoDBEntryException("User does not exist.");
+    	}
+    	
+    	User user = app.getUserByLoginName(loginName);
+    	DateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd 'at' HH:mm:ss z");
+		Date date = Calendar.getInstance().getTime();
+		String created = dateFormat.format(date);
+    	
+    	//create tudoo
+		Tudoo tudoo = new Tudoo();
+		tudoo.setTudooUUID(UUID.randomUUID().toString());
+		tudoo.setTudooOwner(user);
+		tudoo.setCreationDate(created);
+		tudoo.setTitle(title);
+		tudoo.setContent(content);
+		tudoo.setVisibleBy(new ArrayList<String>());
+		tudoo.setEditableBy(new ArrayList<String>());
+
+		persist(tudoo);
 		
 		CriteriaBuilder cb = em.getCriteriaBuilder();
 		CriteriaQuery<Tudoo> cq = cb.createQuery(Tudoo.class);
 		Root<Tudoo> t = cq.from(Tudoo.class);
 		
-		cq.select(t).where(cb.equal(t.get("todooOwner"), managedUser));
+		cq.select(t).where(cb.equal(t.get("tudooUUID"), tudoo.getTudooUUID()));
 		TypedQuery<Tudoo> query = em.createQuery(cq);
 		
-		//TODO
-		return new TudooGroup();
+		return query.getSingleResult();
 	}
 	
 	public ArrayList<Tudoo> getAllVisibleTodoosOfUser(String loginName) {
@@ -69,5 +92,23 @@ public class TudooApp {
 	
 	public void deleteTodoo(UUID todooID) {
 		//TODO
+	}
+	
+	
+	
+	//Helper functions
+	
+	/**
+	 * Helper function to persist an entity in the database. This method does not check for duplicates.
+	 * @param entity some entity that needs to be persisted.
+	 */
+	private void persist(Object entity) {
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		
+		tx.begin();
+		em.persist(entity);
+		tx.commit();
+		em.close();
 	}
 }
