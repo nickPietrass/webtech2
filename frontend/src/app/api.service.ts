ec9aef8fb993059017e1082213ef11c8b14f4029
Nick <nick.pietrass@gmail.com>
2018-07-13 13:31:40 +0200

    npm init & some angular things

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -3,30 +3,50 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class ApiService {
-  localToken = "";
+  localToken = '';
   defaultHeaders = new HttpHeaders({
-    'apiToken': this.localToken
-  })
+    'sessionID': this.localToken
+  });
   constructor(private http: HttpClient) {
   }
-  //check if an API Token is provided / a session exists
+  // check if an API Token is provided / a session exists
   hasToken = () => {
-    if (this.localToken)
+    if (this.localToken) {
       return true;
+    }
     return false;
   }
-  //login to API server
-  sendLoginRequest = (user, password) => {
-    if(!user || !password){
-      console.log("sendLoginRequest needs 2 params, got " + user + " " + password);
+  // login to API server
+  sendLoginRequest = (user, password, callback) => {
+    if (!user || !password) {
+      console.log('sendLoginRequest needs 2 params, got ' + user + ' ' + password);
       return;
     }
-    let loginUrl = "api/users/login";
-    let loginData = {
 
-    }
+    const loginUrl = 'api/users/login';
+    const loginData = {
+
+    };
+    // TODO save auth token
     this.http.post(loginUrl, loginData).subscribe((data) => {
+      console.log(data);
+      callback(data);
+    });
+  }
 
-    })
+  // register a new user
+  registerUser = (user, password, callback) => {
+    // TODO
+  }
+
+  // loads user by Id
+
+  loadUser = (userId) => {
+
+  }
+  // load all Tudoos for user
+  loadAllTodos = () => {
+    // TODO keine params?
+    const getUrl = 'api/tudoos/userTudoos';
   }
 }
