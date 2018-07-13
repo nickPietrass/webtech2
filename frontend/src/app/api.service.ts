e76d520bb6a060b80284e16b362db4d2addff9b3
Nick <nick.pietrass@gmail.com>
2018-07-13 11:35:41 +0200

    api base commit

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -1,10 +1,32 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class ApiService {
-  
+  localToken = "";
+  defaultHeaders = new HttpHeaders({
+    'apiToken': this.localToken
+  })
   constructor(private http: HttpClient) {
   }
+  //check if an API Token is provided / a session exists
+  hasToken = () => {
+    if (this.localToken)
+      return true;
+    return false;
+  }
+  //login to API server
+  sendLoginRequest = (user, password) => {
+    if(!user || !password){
+      console.log("sendLoginRequest needs 2 params, got " + user + " " + password);
+      return;
+    }
+    let loginUrl = "api/users/login";
+    let loginData = {
 
+    }
+    this.http.post(loginUrl, loginData).subscribe((data) => {
+
+    })
+  }
 }
